b6926ea206eb9279abb0799e0851464d3c37d7ee
jonataspcaraujo <J0n4t@s3009>
2020-09-13 23:24:30 -0300

    atualização classe cliente

--- src/exSantanderConta/Cliente.kt
+++ src/exSantanderConta/Cliente.kt
@@ -1,28 +1,49 @@
 package exSantanderConta
 
-class Cliente() {
-    private var codCliente: String = ""
-    private var nomeCliente: String = ""
-    private var rgCliente: String = ""
-    private var cpfCliente: String = ""
+class Cliente(codigo: String = "", nome: String = "", rg: String = "", cpf: String = "") {
+
+    private var codCliente = codigo
+    private var nomeCliente = nome
+    private var rgCliente = rg
+    private var cpfCliente = cpf
 
     init{
-        cadastrarCliente()
+        verificarCadastro()
     }
-    private fun cadastrarCliente() {
-        println("Cadastro de novo cliente")
-        print("Código do Cliente: ")
+    fun verificarCadastro() {
+        println("Verificação de Cadastro de Cliente")
+        if (codCliente == "")   setCodCliente()
+        if (nomeCliente == "")  setNomeCliente()
+        if (rgCliente == "")    setRgCliente()
+        if (cpfCliente == "")   setCpfCliente()
+        println("Cadastro verificado")
+    }
+    private fun setCodCliente() {
+        print("Informe o Código do Cliente: ")
         codCliente = readLine().toString()
-        print("Nome do Cliente: ")
-        nomeCliente = readLine().toString()
-        print("RG do Cliente: ")
-        rgCliente = readLine().toString()
-        print("CPF do Cliente: ")
-        cpfCliente = readLine().toString()
-
-
     }
-
-
-
+    private fun setRgCliente() {
+        print("Informe o RG do Cliente: ")
+        rgCliente = readLine().toString()
+    }
+    private fun setNomeCliente() {
+        print("Informe o Nome do Cliente: ")
+        nomeCliente = readLine().toString()
+    }
+    private fun setCpfCliente() {
+        print("Informe o CPF do Cliente: ")
+        cpfCliente = readLine().toString()
+    }
+    fun getCodCliente():String{
+        return codCliente
+    }
+    fun getNomeCliente():String{
+        return nomeCliente
+    }
+    fun getRgCliente():String{
+        return rgCliente
+    }
+    fun getCpfCliente():String{
+        return cpfCliente
+    }
 }
